80e29749df03bb2edf0dff0de7c12308d4b673cb
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-24 09:18:18 +0530

    commit

---
+++ 2-8.cpp
@@ -0,0 +1,18 @@
+// ex2-8
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    
+    long pop1 = 2425785, pop2 = 47, pop3 = 9761, pop4 = 0;
+
+    cout << setw(8) << "LOCATION"<<setfill('.') << setw(12) << "POPULATION" << endl
+         << setw(8) << "Portcity" << setw(12) << pop1 << endl
+         << setw(8) << "Hightown" << setw(12) << pop2 << endl
+         << setw(8) << "Lowville" << setw(12) << pop3 << endl;
+    
+    return 0;
+}
