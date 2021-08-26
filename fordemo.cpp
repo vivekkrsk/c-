b5122c3cce820084cb84196f897200429ecf236f
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-26 07:46:40 +0530

    commit

---
+++ fordemo.cpp
@@ -0,0 +1,15 @@
+// fordemo.cpp
+// demostrates simple FOR loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j;
+
+    for (j = 0; j < 15; j++)
+        cout << j * j << " " ;
+        cout << endl;
+    return 0;
+}
