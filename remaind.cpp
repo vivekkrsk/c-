36a52661193942a42937c2dfba1b73096051f0fe
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-27 08:42:37 +0530

    commit

---
+++ remaind.cpp
@@ -0,0 +1,14 @@
+// remaind.cpp
+// demostrates remainder oprator
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 6 % 8 << endl
+         << 7 % 8 << endl
+         << 8 % 8 << endl
+         << 9 % 8 << endl
+         << 10 % 8 << endl;
+    return 0;
+}
