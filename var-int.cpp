7678735ab698dbce1364b57957d97231b33bd78c
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-27 08:44:12 +0530

    commit

---
+++ var-int.cpp
@@ -0,0 +1,17 @@
+// int var.cpp
+// demostrates integer variables
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1; //define var1
+    int var2; //define var2
+
+    var1 = 20;               //assign value to var 1
+    var2 = var1 + 10;        //assign value to var 2
+    cout << "var1 + 10 is "; //output text
+    cout << var2 << endl;    //output value of 2
+
+    return 0;
+}
