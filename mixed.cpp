669445e038186a46f7aab9d489096e18a473bcce
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-27 08:42:14 +0530

    commit

---
+++ mixed.cpp
@@ -0,0 +1,16 @@
+//mixed.cpp
+//shows mixed expressions
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 7;
+    float avgWeight = 155.5F;
+
+    double totalweight = count*avgWeight;
+    
+    cout << "totalweight=" << totalweight << endl;
+    return 0;
+}
