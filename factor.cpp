c19b27cf2d80f9e9ebcf95ac9b51908696550baf
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-30 14:30:27 +0530

    commit

--- factor.cpp
+++ factor.cpp
@@ -1,10 +1,20 @@
 // factor.cpp
-// calculate factorial, Demostrate for loop
+// calculate factorial, demostrates for loop
 
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
-    
+    unsigned int numb;
+    unsigned long long int fact = 1;
+
+    cout << "Enter a number: ";
+    cin >> numb;
+
+    for (int j = numb; j > 0; j--)
+        fact *= j;
+    cout << "Factorial is " << fact << endl;
+
+    return 0;
 }
