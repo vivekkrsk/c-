8bfa3d15e3db3e5612a836f80a53e7c53c91c5fc
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-08-30 14:30:02 +0530

    commit

---
+++ endon0.cpp
@@ -0,0 +1,16 @@
+// endon0.cpp
+// demonstrate While loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 99;
+
+    while (n != 0)
+        cin >> n;
+    cout << endl;
+
+    return 0;
+}
