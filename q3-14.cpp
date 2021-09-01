2bd1b27b421b2016a234c2d28e2fb17155d98697
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-01 20:56:53 +0530

    commit

---
+++ q3-14.cpp
@@ -0,0 +1,15 @@
+// q3-14.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age>21)
+      cout << "Yes"<< endl;
+    return 0;
+}
