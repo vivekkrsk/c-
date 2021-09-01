3052ebeb3414fa569d2ca95a0534c84f3d158301
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-01 20:56:22 +0530

    commit

---
+++ q3-13.cpp
@@ -0,0 +1,16 @@
+// q3-13.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=100;
+     
+     do{
+         cout <<i<<endl;
+         i++;
+     }while(i<=110);
+
+    return 0;
+}
