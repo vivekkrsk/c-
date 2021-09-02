ce01ce5049ec6e6ea9a7cd025614e81f35ed7643
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-02 11:00:31 +0530

    commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+switch (dir)
+        {
+        case 'n':
+            y--;
+            break;
+
+        case 's':
+            y++;
+            break;
+
+        case 'e':
+            x++;
+            break;
+
+        case 'w':
+            x--;
+            break;
+        }
