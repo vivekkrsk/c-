783f76881d352a8d41b7cbf04d3787c93df34578
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-02 10:59:42 +0530

    commit

---
+++ relate.cpp
@@ -0,0 +1,18 @@
+// relat.cpp
+// demonstrate relational operators
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numb;
+
+    cout << "Enter a number: ";
+    cin >> numb;
+    cout << "numb < 10  is " << (numb < 10) << endl;
+    cout << "numb > 10  is " << (numb > 10) << endl;
+    cout << "numb == 10  is " << (numb == 10) << endl;
+
+    return 0;
+}
