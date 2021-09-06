b9fb053b812d5beb1bb1b812f7a51c3fd46442ce
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-06 08:46:32 +0530

    commit

---
+++ nesting2.cpp
@@ -0,0 +1,73 @@
+// nesting of member functionin oops
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+
+void binary ::read(void)
+{
+    cout << "Enter a binary number: ";
+    cin >> s;
+}
+
+void binary ::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "incorrect Binary number";
+            exit(0);
+        }
+
+    }
+}
+
+void binary :: ones_compliment(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        
+        {
+            s.at(i) = '0';
+        }
+        
+    }
+}
+
+void binary :: display(void)
+{
+    cout << "Displaying your binary number"<<endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    binary b;
+    b.read();
+    b.chk_bin();
+    b.display();
+    b.ones_compliment();
+    b.display();
+    return 0;
+}
