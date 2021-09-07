5897ff0c2685622605fe06506cfd036614c0df38
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-07 19:49:26 +0530

    commit

---
+++ ch5-q17.cpp
@@ -0,0 +1,19 @@
+// ch5-q17
+
+#include <iostream>
+using namespace std;
+
+int times(int a)
+{
+    return (a*2);
+}
+
+int main()
+{
+    int b;
+    b=times(2);
+
+    cout << b;
+
+    return 0;
+}
