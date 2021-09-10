a86fad6fbf4a95d127a5bff1854c784c43f2fa11
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-10 13:55:18 +0530

    commit

---
+++ reforder.cpp
@@ -0,0 +1,33 @@
+//reforder.cpp
+//orders two argument passed by reference
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    void order(int &, int &);
+
+    int n1 = 99, n2 = 11;
+    int n3 = 22, n4 = 88;
+
+    order(n1, n2);
+    order(n3, n4);
+
+    cout << "n1=" << n1 << endl;
+    cout << "n2=" << n2 << endl;
+    cout << "n3=" << n3 << endl;
+    cout << "n4=" << n4 << endl;
+
+    return 0;
+}
+
+void order(int &numb1, int &numb2)
+{
+    if (numb1 > numb2)
+    {
+        int temp = numb1;
+        numb1 = numb2;
+        numb2 = temp;
+    }
+}
