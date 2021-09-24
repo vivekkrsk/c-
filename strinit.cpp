37bc959e77b4baa28e14a1568da9afa47665c92d
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-09-24 18:10:16 +0530

    commit

---
+++ strinit.cpp
@@ -0,0 +1,14 @@
+//strinit.cpp
+// initialized string
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[] = "Farewell! thou art too dear for my possessing.";
+    
+    cout << str << endl;
+
+    return 0;
+}
