434490bd76d4cacc2eb9a98e4bbfdc46a9782ccf
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-10-01 14:31:51 +0530

    commit

---
+++ linklist.cpp
@@ -0,0 +1,56 @@
+// linkedlist.cpp
+// linked list
+
+#include <iostream>
+using namespace std;
+
+struct link
+{
+    int data;
+    link* next;
+};
+
+class linklist
+{
+private:
+    link* first;
+public:
+linklist()
+{ first = NULL;}
+void additem(int d);
+void display();
+   
+};
+
+void linklist::additem(int d)
+{
+    link* newlink = new link;
+    newlink -> data=d;
+    newlink -> next = first;
+    first = newlink;
+}
+
+void linklist::display()
+{
+    link* current = first;
+    while (current != NULL)
+    {
+        cout << current ->data << endl;
+        current = current -> next;
+    }
+    
+}
+
+int main()
+{
+    linklist li;
+
+    li.additem(25);
+    li.additem(36);
+    li.additem(49);
+    li.additem(64);
+
+    li.display();
+
+    return 0;
+}
