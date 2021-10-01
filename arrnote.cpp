cb00460155650cd0b18ea38eb3a8efc901373288
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-10-01 14:30:16 +0530

    commit

---
+++ arrnote.cpp
@@ -0,0 +1,17 @@
+// arrnote.cpp
+// array accessed with array notation
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int intarray[5]= { 31,54,77,52,93 };
+
+    for(int j=0; j<5; j++)
+    {
+        cout << intarray[j] << endl;
+    }
+
+    return 0;
+}
