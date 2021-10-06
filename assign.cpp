6745872154943b2d23b131246c2b78eb52eac618
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-10-06 17:59:05 +0530

    commit

--- assign.cpp
+++ assign.cpp
@@ -1,22 +1,46 @@
-//asssign.cpp
-//demostrates arthemitic assignment operator
+// assign.cpp
+// overloads assignment operator (=)
 
 #include <iostream>
 using namespace std;
 
+class alpha
+{
+private:
+    int data;
+
+public:
+    alpha()
+    {
+    }
+    alpha(int d)
+    {
+        data = d;
+    }
+    void display()
+    {
+        cout << data;
+    }
+    alpha operator=(alpha& a)
+    {
+        data = a.data;
+        cout << "\nAssignment operator invoked";
+    }
+};
+
 int main()
 {
-    int ans = 27;
+    alpha a1(37);
+    alpha a2;
+    a2 = a1;
 
-    ans += 10;
-    cout << ans << ", ";
-    ans -= 7;
-    cout << ans << ", ";
-    ans *= 2;
-    cout << ans << ", ";
-    ans /= 3;
-    cout << ans << ", ";
-    ans %= 3;
-    cout << ans << ", ";
+    cout << "\na2 = ";
+    a2.display();
+
+    alpha a3 = a2;
+    cout << "\na3 = ";
+    a3.display();
+    cout << endl;
+
     return 0;
 }
