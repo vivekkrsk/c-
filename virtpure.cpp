6288c0c64a295dba2be50a10dbbec70c7eec67a0
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-10-09 06:50:39 +0530

    commit

---
+++ virtpure.cpp
@@ -0,0 +1,42 @@
+// virtpure.cpp
+// pure virtual function
+
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    public: 
+      virtual void show() =0;
+};     
+
+class Derv1 : public Base
+{
+    public: 
+    void show()
+    { cout << "Derv1\n"; }
+};
+
+class Derv2 : public Base
+{ 
+    public: 
+     void show()
+     {  cout << "Derv2\n"; }
+};
+
+
+int main()
+{
+    // base bad;    can't make class from abstract class
+    Base* arr[2];
+    Derv1 dv1;
+    Derv2 dv2;
+
+    arr[0]= &dv1;
+    arr[1]= &dv2;
+
+    arr[0]-> show();
+    arr[1]-> show();
+
+    return 0;
+}
