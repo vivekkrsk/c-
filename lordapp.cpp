49857830e642cf7b32acce291465a7d6c5da36e4
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-10-27 18:58:13 +0530

    commit

---
+++ lordapp.cpp
@@ -0,0 +1,12 @@
+// lordapp.cpp
+// client file for apart program
+
+#include "landlord.h"
+
+int main()
+{
+    userInterface theUserInterface;
+    
+    theUserInterface.interact();
+    return 0;
+}
