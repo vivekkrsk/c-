88bb8035d4c920d1963004f52a22437342dc7559
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-11-01 19:04:30 +0530

    commit

---
+++ find15.cpp
@@ -0,0 +1,18 @@
+// find.cpp
+// find the first object with a specified value
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[] = {11, 22, 33, 44, 55, 66, 77, 88};
+
+int main()
+{
+    int *ptr;
+    ptr = find(arr, arr + 8, 33);
+    cout << "First object with value 33 founr at offset "
+         << (ptr - arr) << endl;
+
+    return 0;
+}
