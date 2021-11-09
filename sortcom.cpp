13627c5438d841b888dee7b485c5434658605cf5
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-11-09 17:40:17 +0530

    commit

---
+++ sortcom.cpp
@@ -0,0 +1,30 @@
+// sortcom.cpp
+// sort array of string with user - written comparison function
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+char* names[] = { "George", "penny", "Estelle", "Don", "Mike", "Bob"};
+
+bool alpha_comp(char*, char*);
+
+int main()
+{
+    sort( names,names+6,alpha_comp);
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << names[i] << endl;
+    }
+    
+
+    return 0;
+}
+
+bool alpha_comp(char* s1, char* s2)
+{
+    return (strcmp(s1,s2)<0)? true: false;
+}
