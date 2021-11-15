efc65a6181b552271143e9e95e443317c84e5106
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-11-15 17:21:53 +0530

    commit

---
+++ pattern3.cpp
@@ -0,0 +1,18 @@
+// pattern3.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      for(int j=1; j<=10; j++)
+    {
+        for(int i=0; i<(10-j); i++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+   
+    return 0;
+}
