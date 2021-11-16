c4bf80bada692218cad30116d0a10ff65de552e1
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-11-16 18:05:57 +0530

    commit

---
+++ pattern4.cpp
@@ -0,0 +1,22 @@
+// pattern4.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      for(int j=1; j<=10; j++)
+    {
+        for(int i=0; i<j; i++)
+        {
+            cout << " ";
+        }
+        for(int i=0; i<(10-j); i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
