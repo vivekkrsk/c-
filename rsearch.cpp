ef8535d44c9a229f8296867c789536c34ccde7b5
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-12-05 06:49:15 +0530

    commit

---
+++ rsearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int source[] = {11,44,33,11,22,33,11,22,44};
+int pattern[]= {11,22,33};
+
+int main()
+{
+    int *ptr;
+    ptr= search(source,source+9,pattern,pattern+3);
+
+    if (ptr==source+9)
+    {
+        cout << "No match Found\n";
+    }
+    else
+    cout<<"Match at "<<(ptr - source)<<endl;
+
+    return 0;
+    
+}
